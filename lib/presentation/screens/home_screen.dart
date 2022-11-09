3cc2485ba4008b040a61b333f64a32b9458bca04
nikchuba <nikchuba@gmail.com>
2022-11-09 18:58:09 +0400

    implements draggable objects

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:html';
 import 'dart:math';
 
 import 'package:flutter/foundation.dart';
@@ -37,7 +38,8 @@ class _HomeScreenState extends State<HomeScreen> {
   bool verbose = true;
   bool disposed = false;
 
-  late three.Object3D object;
+  late three.Object3D ground;
+  late three.Object3D draggableObject;
 
   // late three.Texture texture;
 
@@ -52,6 +54,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   final GlobalKey<three_jsm.DomLikeListenableState> globalKey = GlobalKey<three_jsm.DomLikeListenableState>();
   late three_jsm.OrbitControls controls;
+  late three.Raycaster raycaster;
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
@@ -206,12 +209,11 @@ class _HomeScreenState extends State<HomeScreen> {
   void initPage() async {
     final aspectRatio = width / height;
 
-    camera = three.PerspectiveCamera(45, aspectRatio, 0.1, 1000);
+    scene = three.Scene()
+      ..background = three.Color.setRGB255(255, 255, 255)
+      ..rotateX(-three.Math.PI / 2);
 
-    scene = three.Scene();
-    scene.background = three.Color.setRGB255(255, 255, 255);
-    scene.rotateX(-three.Math.PI / 2);
-    camera
+    camera = three.PerspectiveCamera(45, aspectRatio, 0.1, 1000)
       ..position.set(45, 30, 60)
       ..lookAt(scene.position);
 
@@ -223,147 +225,194 @@ class _HomeScreenState extends State<HomeScreen> {
       ..dollyIn(0.3)
       ..update();
 
-    final ground = await create3DObject(
+    raycaster = three.Raycaster();
+
+    ground = await create3DObject(
       width: 100,
       length: 100,
       asset: 'assets/grass.png',
       textureRepeat: true,
       countRepeat: 25,
     );
-    final box1 = await create3DObject(
-      width: 2,
-      length: 2,
-      height: 2,
-      position: three.Vector3(10, 0, 0),
-      color: Colors.amber,
-      castShadow: true,
-    );
-    final box2 = await create3DObject(
-      width: 1.5,
-      length: 1.5,
-      height: 1.5,
-      position: three.Vector3(9, 2, 0),
-      color: Colors.redAccent,
-      castShadow: true,
-    );
+
     var containerBottom = await create3DObject(
-      width: truckSize.y,
-      length: truckSize.x,
-      height: 0.01,
-      rotation: three.Vector3(0, 0, pi / 2),
-      asset: 'assets/truck_container.jpg',
-    );
-    var containerFront = await create3DObject(
-      width: truckSize.x,
-      length: truckSize.z,
-      position: three.Vector3(0, truckSize.y / 2, truckSize.z / 2),
-      rotation: three.Vector3(pi / 2, 0, 0),
-      asset: 'assets/truck_container.jpg',
-    );
-    var containerRight = await create3DObject(
-      width: truckSize.y,
-      length: truckSize.z,
-      position: three.Vector3(truckSize.x / 2, 0, truckSize.z / 2),
-      rotation: three.Vector3(pi / 2, -pi / 2, 0),
-      asset: 'assets/truck_container.jpg',
-    );
-    var containerLeft = await create3DObject(
-      width: truckSize.y,
-      length: truckSize.z,
-      position: three.Vector3(-truckSize.x / 2, 0, truckSize.z / 2),
-      rotation: three.Vector3(pi / 2, pi / 2, 0),
-      asset: 'assets/truck_container.jpg',
-    );
-    var containerBack = await create3DObject(
-      width: truckSize.x,
-      length: truckSize.z,
-      position: three.Vector3(0, -truckSize.y / 2, truckSize.z / 2),
-      rotation: three.Vector3(pi / 2, pi, 0),
-      asset: 'assets/truck_container.jpg',
-    );
+          width: truckSize.y,
+          length: truckSize.x,
+          height: 0.01,
+          rotation: three.Vector3(0, 0, pi / 2),
+          asset: 'assets/truck_container.jpg',
+        ),
+        containerFront = await create3DObject(
+          width: truckSize.x,
+          length: truckSize.z,
+          position: three.Vector3(0, truckSize.y / 2, truckSize.z / 2),
+          rotation: three.Vector3(pi / 2, 0, 0),
+          asset: 'assets/truck_container.jpg',
+        ),
+        containerRight = await create3DObject(
+          width: truckSize.y,
+          length: truckSize.z,
+          position: three.Vector3(truckSize.x / 2, 0, truckSize.z / 2),
+          rotation: three.Vector3(pi / 2, -pi / 2, 0),
+          asset: 'assets/truck_container.jpg',
+        ),
+        containerLeft = await create3DObject(
+          width: truckSize.y,
+          length: truckSize.z,
+          position: three.Vector3(-truckSize.x / 2, 0, truckSize.z / 2),
+          rotation: three.Vector3(pi / 2, pi / 2, 0),
+          asset: 'assets/truck_container.jpg',
+        ),
+        containerBack = await create3DObject(
+          width: truckSize.x,
+          length: truckSize.z,
+          position: three.Vector3(0, -truckSize.y / 2, truckSize.z / 2),
+          rotation: three.Vector3(pi / 2, pi, 0),
+          asset: 'assets/truck_container.jpg',
+        );
+    var truckContainer = [containerBottom, containerRight, containerLeft, containerFront, containerBack];
 
-    scene.addAll([containerBottom, containerRight, containerLeft, containerFront, containerBack]);
+    var textMaterial = three.LineBasicMaterial()..color = three.Color.setRGB255(0, 0, 0);
+    var linePointsLH = [
+          three.Vector3(truckSize.x, -truckSize.y / 2, 0),
+          three.Vector3(truckSize.x, truckSize.y / 2, 0),
+          three.Vector3(truckSize.x, truckSize.y / 2, truckSize.z),
+        ],
+        linePointsW = [
+          three.Vector3(-truckSize.x / 2, -truckSize.y / 2 - truckSize.x / 2, 0),
+          three.Vector3(truckSize.x / 2, -truckSize.y / 2 - truckSize.x / 2, 0),
+        ];
+    var lineGeometryLH = three.BufferGeometry().setFromPoints(linePointsLH),
+        lineGeometryW = three.BufferGeometry().setFromPoints(linePointsW);
+    var lineLH = three.Line(
+          lineGeometryLH,
+          textMaterial,
+        ),
+        lineW = three.Line(
+          lineGeometryW,
+          textMaterial,
+        );
+    var lines = [lineLH, lineW];
 
-    var pointsLH = [
-      three.Vector3(truckSize.x, -truckSize.y / 2, 0),
-      three.Vector3(truckSize.x, truckSize.y / 2, 0),
-      three.Vector3(truckSize.x, truckSize.y / 2, truckSize.z),
-    ];
-    var pointsW = [
-      three.Vector3(-truckSize.x / 2, -truckSize.y / 2 - truckSize.x / 2, 0),
-      three.Vector3(truckSize.x / 2, -truckSize.y / 2 - truckSize.x / 2, 0),
-    ];
-    var geometryLH = three.BufferGeometry().setFromPoints(pointsLH);
-    var arrowLH = three.Line(
-      geometryLH,
-      three.LineBasicMaterial()..color = three.Color.setRGB255(0, 0, 0),
-    );
-    var geometryW = three.BufferGeometry().setFromPoints(pointsW);
-    var arrowW = three.Line(
-      geometryW,
-      three.LineBasicMaterial()..color = three.Color.setRGB255(0, 0, 0),
-    );
     var textWidth = await createText(
-      '${truckSize.x * 1000}',
-      three.Vector3(truckSize.x / 2, -truckSize.y / 2 - truckSize.x / 1.5, 0),
-    );
-    var textLength = await createText(
-      '${truckSize.y * 1000}',
-      three.Vector3(truckSize.x * 1.2, -truckSize.y / 2, 0),
-      three.Vector3(0, 0, pi / 2),
-    );
-    var textHeight = await createText(
-      '${truckSize.z * 1000}',
-      three.Vector3(truckSize.x, truckSize.y / 2, truckSize.z),
-      three.Vector3(pi / 2, pi / 4),
-    );
-    var textZeroLH = await createText(
-      '0',
-      three.Vector3(truckSize.x * 1.2, truckSize.y / 2 - 0.2, 0),
-      three.Vector3(0, 0, pi / 2),
-    );
-    var textZeroW = await createText(
-      '0',
-      three.Vector3(-truckSize.x / 2 - 0.2, -truckSize.y / 2 - truckSize.x / 1.5, 0),
-    );
+          '${truckSize.x * 1000}',
+          three.Vector3(truckSize.x / 2, -truckSize.y / 2 - truckSize.x / 1.5, 0),
+        ),
+        textLength = await createText(
+          '${truckSize.y * 1000}',
+          three.Vector3(truckSize.x * 1.2, -truckSize.y / 2, 0),
+          three.Vector3(0, 0, pi / 2),
+        ),
+        textHeight = await createText(
+          '${truckSize.z * 1000}',
+          three.Vector3(truckSize.x, truckSize.y / 2, truckSize.z),
+          three.Vector3(pi / 2, pi / 4),
+        ),
+        textZeroLH = await createText(
+          '0',
+          three.Vector3(truckSize.x * 1.2, truckSize.y / 2 - 0.2, 0),
+          three.Vector3(0, 0, pi / 2),
+        ),
+        textZeroW = await createText(
+          '0',
+          three.Vector3(-truckSize.x / 2 - 0.2, -truckSize.y / 2 - truckSize.x / 1.5, 0),
+        );
+    var texts = [textWidth, textLength, textHeight, textZeroLH, textZeroW];
 
-    scene.addAll([textWidth, textLength, textHeight, textZeroLH, textZeroW]);
-    scene.addAll([ground, box1, box2, arrowLH, arrowW]);
+    var box1 = await create3DObject(
+          width: 2,
+          length: 2,
+          height: 2,
+          position: three.Vector3(10, 0, 0),
+          color: Colors.amber,
+          castShadow: true,
+        ),
+        box2 = await create3DObject(
+          width: 1.5,
+          length: 1.5,
+          height: 1.5,
+          position: three.Vector3(9, 2, 0),
+          color: Colors.redAccent,
+          castShadow: true,
+        ),
+        box3 = await create3DObject(
+          width: 2.5,
+          length: 2.5,
+          height: 1.5,
+          position: three.Vector3(6, 0, 0),
+          color: Colors.purple,
+          castShadow: true,
+        ),
+        box4 = await create3DObject(
+          width: 2,
+          length: 4.5,
+          height: 2.5,
+          position: three.Vector3(5, -4, 0),
+          color: Colors.cyan,
+          castShadow: true,
+        );
+    var boxes = [box1, box2, box3, box4];
 
     var light = three.PointLight(three.Color.setRGB255(255, 255, 255), 1)
-      ..position.z = 30
-      ..position.x = 30
-      ..position.y = 30
-      ..castShadow = true;
+          ..position.z = 30
+          ..position.x = 30
+          ..position.y = 30
+          ..castShadow = true,
+        ambientLight = three.AmbientLight(three.Color(0xffffff), 0.3);
+    var lights = [light, ambientLight];
 
-    scene.addAll([light, three.AmbientLight(three.Color(0xffffff), 0.3)]);
+    scene
+      ..add(ground)
+      ..addAll(truckContainer)
+      ..addAll(lines)
+      ..addAll(texts)
+      ..addAll(boxes)
+      ..addAll(lights);
 
-    // var raycaster = three.Raycaster();
-    // renderer?.domElement.addEventListener(
-    //   'mousedown',
-    //   (event) {
-    //     final vector = three.Vector2(
-    //       -(width / 2 - event.offset.x) * 2 / width,
-    //       (height / 2 - event.offset.y) * 2 / height,
-    //     );
-    //     raycaster.setFromCamera(vector, camera);
-    //     var intersects = raycaster.intersectObject(ground, false);
-    //     if (intersects.isNotEmpty) {
-    //       // cube
-    //       //   ..rotateX(pi / 4)
-    //       //   ..rotateY(pi / 4);
-    //       counter += 1;
-    //       print(intersects.length);
-    //       print(intersects[0].object.id);
-    //       setState(() {});
-    //     }
-    //     // print('Z: ${vector.z}');
-    //   },
-    //   true,
-    // );
+    void dragObject(event) {
+      final point = pointToVector2(event.offset);
+      raycaster.setFromCamera(point, camera);
+      var intersect = raycaster.intersectObject(ground, false);
+      if (intersect.isNotEmpty) {
+        draggableObject.position.set(
+          intersect.first.point.x,
+          -intersect.first.point.z,
+        );
+      }
+    }
+
+    renderer?.domElement.addEventListener(
+      'mousedown',
+      (event) {
+        final vector = pointToVector2(event.offset);
+        raycaster.setFromCamera(vector, camera);
+        var intersects = raycaster.intersectObjects(boxes, false);
+        if (intersects.isNotEmpty) {
+          controls.enabled = false;
+          draggableObject = intersects.first.object;
+          renderer!.domElement.addEventListener('mousemove', dragObject, true);
+        }
+      },
+      true,
+    );
+    renderer?.domElement.addEventListener(
+      'mouseup',
+      (event) {
+        renderer!.domElement.removeEventListener('mousemove', dragObject, true);
+        controls.enabled = true;
+      },
+      true,
+    );
 
     loaded = true;
     animate();
+  }
+
+  three.Vector2 pointToVector2(Point point) {
+    return three.Vector2(
+      -(width / 2 - point.x) * 2 / width,
+      (height / 2 - point.y) * 2 / height,
+    );
   }
 
   Future<three.Font> loadFont() async {
